9d7b91b874ced239c4d56beb870f3cb1fcbf5bee
gamegrd <gamegrd@gmail.com>
2023-12-01 14:01:27 +0800

    tran

--- src/features/assistant/ipc-api/tran.ts
+++ src/features/assistant/ipc-api/tran.ts
@@ -1,16 +1,16 @@
-import { ipcMain, ipcRenderer } from 'electron';
+import { ipcRenderer } from 'electron';
 
 export default async () => {
-  ipcMain.handle('assistant:log', async (_event, ...msg: any) => {
-    console.warn('handle:assistant:log', ...msg);
-    return 'Call Ok';
-  });
+  // ipcMain.handle('assistant:log', async (_event, ...msg: any) => {
+  //   console.warn('handle:assistant:log', ...msg);
+  //   return 'Call Ok';
+  // });
 
-  ipcMain.handle('assistant:token', async (_event, ...msg: any) => {
-    console.warn('assistant:token', ...msg);
-    debugger;
-    return 'token is abfc';
-  });
+  // ipcMain.handle('assistant:token', async (_event, ...msg: any) => {
+  //   console.warn('assistant:token', ...msg);
+  //   debugger;
+  //   return 'token is abfc';
+  // });
 
   ipcRenderer.on('assistant:log', (_e, arg) => {
     console.warn('assistant:log', _e, ...arg);

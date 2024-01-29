d9c1a1b575228b689937970534f22983d8eb5c86
gamegrd <gamegrd@gmail.com>
2024-01-29 22:28:21 +0800

    lint

--- src/webview/preload.ts
+++ src/webview/preload.ts
@@ -1,10 +1,1 @@
-interface Window {
-  xgVer: string;
-  chrome: {
-    notifications: {};
-    window: {};
-    versuion: string;
-  };
-}
-
-window.xgVer = 'xgDebug 1.03';
+window['xgVer'] = 'xgDebug 1.03';
